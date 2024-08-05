20de80bff092121fc844398c80b90c435f2ca260
melkijo <jonathanandaraa@gmail.com>
2024-08-06 00:21:11 +0800

    fix: dummy data

--- data/dummyData.ts
+++ data/dummyData.ts
@@ -1,89 +1,13 @@
 import { StoryType } from "@/types/storyType";
 const exampleData: StoryType[] = [
     {
-      id: 1,
-      date: "Sabtu Juli 13",
-      time: "10:53",
-      mood: "senang",
-      color: "bg-[#DE5F5F]/40",
-    },
-    {
-      id: 2,
-      date: "Minggu Juli 14",
-      time: "12:30",
-      mood: "sedih",
-      color: "bg-[#5FDE5F]/40",
-    },
-    {
-      id: 3,
-      date: "Senin Juli 15",
-      time: "09:45",
-      mood: "biasa",
-      color: "bg-[#5F5FDE]/40",
-    },
-    {
-      id: 4,
-      date: "Sabtu Juli 13",
-      time: "10:53",
-      mood: "senang",
-      color: "bg-[#DE5F5F]/40",
-    },
-    {
-      id: 5,
-      date: "Minggu Juli 14",
-      time: "12:30",
-      mood: "sedih",
-      color: "bg-[#5FDE5F]/40",
-    },
-    {
-      id: 6,
-      date: "Senin Juli 15",
-      time: "09:45",
-      mood: "biasa",
-      color: "bg-[#5F5FDE]/40",
-    },
-    {
-        id: 7,
-        date: "Sabtu Juli 13",
-        time: "10:53",
-        mood: "senang",
-        color: "bg-[#DE5F5F]/40",
-      },
-      {
-        id: 8,
-        date: "Minggu Juli 14",
-        time: "12:30",
-        mood: "sedih",
-        color: "bg-[#5FDE5F]/40",
-      },
-      {
-        id: 9,
-        date: "Senin Juli 15",
-        time: "09:45",
-        mood: "biasa",
-        color: "bg-[#5F5FDE]/40",
-      },
-      {
-        id: 10,
-        date: "Sabtu Juli 13",
-        time: "10:53",
-        mood: "senang",
-        color: "bg-[#DE5F5F]/40",
-      },
-      {
-        id: 11,
-        date: "Minggu Juli 14",
-        time: "12:30",
-        mood: "sedih",
-        color: "bg-[#5FDE5F]/40",
-      },
-      {
-        id: 12,
-        date: "Senin Juli 15",
-        time: "09:45",
-        mood: "biasa",
-        color: "bg-[#5F5FDE]/40",
-      },
+        email: "",
+        feel: "senang",
+        photo: "",
+        story: "Hari ini saya senang karena cuaca cerah",
+        storyDate: "2021-08-01"
+    }
+      
   ];
 
   export {exampleData};
